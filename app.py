a216c7fb0d46156f27a3c7506e979ce044f467f7
youricolera <youri.colera@epita.fr>
2022-12-14 00:10:04 +0100

    add api

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
-from typing import Union
 from pydantic import BaseModel
+from typing import Union
+import fasttext
+
+model = fasttext.load_model('imdb_model.bin')
 
 app = FastAPI()
 
 class Item(BaseModel):
-    
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get('/hello')
 def read_root():
@@ -13,4 +18,13 @@ def read_root():
 
 @app.get('/item/{item_id}')
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {'item_id': item_id, 'q': q}
+
+@app.put('/item/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': item_id}
+
+@app.post('/predict')
+def predict_model(text: str):
+    pred = model.predict(text)
+    return {'prediction': pred[0][0], 'confidence': pred[1][0]}
