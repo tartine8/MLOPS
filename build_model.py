0a6bc55cd1f38f8cd730f17d3f6a128fd67d361d
youricolera <youri.colera@epita.fr>
2022-12-14 08:49:25 +0100

    change model + clean code

--- build_model.py
+++ build_model.py
@@ -1,47 +1,22 @@
-from datasets import load_dataset
-from string import punctuation
-import fasttext
-import random
-
-def preprocess(arr):
-    return list(map(lambda t: t.lower().translate(str.maketrans(dict.fromkeys(punctuation))), arr))
-
-def fasttext_file(filename, x, y):
-    file = open(filename, 'w')
-    for i in range(len(x)):
-        file.write(f"__label__{'positive' if y[i] == 1 else 'negative'} {x[i]}\n")
-    file.close()
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import joblib
+import pandas as pd
 
 def build_model():
-    #Load data
-    data = load_dataset("imdb")
+    data = pd.read_csv('diabetes.csv')
+    data = data.rename(columns={'Glucose': 'glucose', 'BMI': 'bmi', 'Age': 'age'})
 
-    #Shuffle data
-    tmp = dict(zip(data['train'][:]['text'], data['train'][:]['label']))
-    keys = list(tmp.keys())
-    random.shuffle(keys)
-    tmp = [(key, tmp[key]) for key in keys]
+    X = data[['glucose', 'bmi', 'age']]
+    y = data['Outcome']
 
-    #Split data
-    X_train = [key for key, val in tmp]
-    y_train = [val for key, val in tmp]
-    X_test = data["test"][:]["text"]
-    y_test = data["test"][:]["label"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    #preprocess data
-    X_train = preprocess(X_train)
-    X_test = preprocess(X_test)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    print('score:', model.score(X_test, y_test))
 
-    #Convert data to fasttext files
-    fasttext_file('train.txt', X_train, y_train)
-    fasttext_file('test.txt', X_test, y_test)
-
-    #Train and test model
-    model = fasttext.train_supervised(input='train.txt')
-    print('train values: ', model.test('train.txt'))
-    print('test values: ', model.test('test.txt'))
-
-    #Save model
-    model.save_model('imdb_model.bin')
+    joblib.dump(model, 'model.joblib')
+    joblib.dump(X_train, 'xtrain.joblib')
 
 build_model()
